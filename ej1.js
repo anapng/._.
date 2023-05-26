b35e17e3f71b3e77637dcacadc5e4b33e86dad8a
anapng <134026852+anapng@users.noreply.github.com>
2023-05-25 20:38:11 -0600

    Update ej1.js

--- ej1.js
+++ ej1.js
@@ -1,1 +1,30 @@
+    public static void main(String[] args) {
+        
+        //Variables
+        int num;
+        int num2;  
+        int num3;
+        int num4;
+        float resSuma;
+        double resPromedio;
+        
+        //Introduccion datos
+        num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+        num3 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+        num4 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+        
+        //Proceso de datos
+        PesSuma = num + num2 + num3 + num4;
+        resPromedio = resSuma / 4;
 
+        //Salida datos
+        System.out.println("La suma total de los datos es: "+resSuma);
+        System.out.println("El promedio de los datos es: "+resPromedio);
+        
+        
+        //trabajo 2
+        String nombre = "";
+        
+        nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
+       JOptionPane.showMessageDialog(null,("Hola " +nombre+ ", bienvenida a este programa desarrollado en JAVA con Netbeans."));
