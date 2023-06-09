184007eb6beba7d7d51740f9ff141bdc41e8f90f
anapng <134026852+anapng@users.noreply.github.com>
2023-06-08 20:46:34 -0600

    Create ._.h

---
+++ ._.h
@@ -0,0 +1,14 @@
+public class Clase4 {
+
+    public static void main(String[] args) {
+        String Simbolo = "@@@@@@@@@@";
+        for(int i = 0; i < 5; i++ ){
+            System.out.println(Simbolo);
+        }
+        int i = 0;
+        while (i < 5){
+            System.out.println(Simbolo);
+            i++;
+        }
+    }
+}
